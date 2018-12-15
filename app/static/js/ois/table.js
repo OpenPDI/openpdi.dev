8387c24ecc22112f808e9d2d8ee72cd83785807e
Joseph Kato <joseph@jdkato.io>
2018-12-14 16:10:04 -0800

    Update table styling

--- app/static/js/ois/table.js
+++ app/static/js/ois/table.js
@@ -1,4 +1,11 @@
+$.fn.dataTable.ext.classes.sPageButton = 'button is-medium'
 var database = $('#database').DataTable({
+  responsive: true,
+  scrollY: '300px',
+  scrollCollapse: true,
+  searching: false,
+  pageLength: 50,
+  lengthChange: false,
   order: [[ 0, 'desc' ]],
   language: {
     'zeroRecords': 'No matching records found.'
@@ -25,11 +32,19 @@ var database = $('#database').DataTable({
   'columnDefs': [
     { type: 'date', targets: 0 },
     { type: 'num', targets: 2 }
-  ],
-  dom: 'Blfrtip',
+  ]
+  /* dom: 'lfrtip',
   buttons: [
-    'csvHtml5',
-    'pdfHtml5'
+    {
+      extend: 'csvHtml5',
+      text: 'Export as CSV',
+      className: 'button is-medium is-rounded is-light'
+    },
+    {
+      extend: 'pdfHtml5',
+      text: 'Export as PDF',
+      className: 'button is-medium is-rounded is-light'
+    }
   ],
   'oTableTools': {
     'aButtons': [
@@ -40,5 +55,5 @@ var database = $('#database').DataTable({
         'oSelectorOpts': { filter: 'applied', order: 'current' }
       }
     ]
-  }
+  } */
 })
