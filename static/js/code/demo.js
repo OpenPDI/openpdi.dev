368543e67ad0f4ae745ae04a2aa2ff946ae09cbe
Joseph Kato <joseph@jdkato.io>
2020-07-10 16:11:56 -0700

    WIP

---
+++ static/js/code/demo.js
@@ -0,0 +1,7 @@
+new Juniper({
+    repo: 'OpenPDI/demo',
+    theme: 'github-dark',
+    classNames: {
+        button: 'juniper-button btn btn-sm btn-primary',
+    }
+})
---
+++ static/js/code/typed.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typed', {
+    strings: ["pip install openpdi"],
+    typeSpeed: 80,
+    loop: true,
+    backDelay: 2000
+});
